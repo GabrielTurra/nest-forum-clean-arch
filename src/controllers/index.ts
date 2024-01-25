9a0c3e4f1a59a89eee07eb8166299bc8f645b88c
Gabriel Turra <gabriel.turra@outlook.com>
2024-01-25 15:54:38 -0300

    feat(questions): add fetch-recent-questions controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,13 @@
+// All Controllers defined here will be imported into the app.module.
+
 import { AuthenticateController } from "./authenticate.controller";
 import { CreateAccountController } from "./create-account.controller";
 import { CreateQuestionController } from "./create-question.controller";
+import { FetchRecentQuestionsController } from "./fetch-recent-questions.controller";
 
 export const GlobalControllers = [
   CreateAccountController,
   CreateQuestionController,
   AuthenticateController,
+  FetchRecentQuestionsController,
 ];
