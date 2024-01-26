76c4b39c17e0f16835bf3de99ed2cd8b143340ac
Gabriel Turra <gabriel.turra@outlook.com>
2024-01-26 14:33:14 -0300

    feat(repositories): implement questions repository methods

--- src/infra/database/prisma/mappers/prima-question-mapper.ts
+++ src/infra/database/prisma/mappers/prima-question-mapper.ts
@@ -1,5 +1,5 @@
 import { Question } from "@/domain/forum/enterprise/entities/question";
-import { Question as PrismaQuestion } from "prisma/prisma-client";
+import { Question as PrismaQuestion, Prisma } from "prisma/prisma-client";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
@@ -11,11 +11,24 @@ export class PrismaQuestionMapper {
         content: raw.content,
         slug: Slug.create(raw.slug),
         authorId: new UniqueEntityID(raw.authorId),
-        bestAnswerId: undefined,
+        bestAnswerId: raw.bestAnswerId ? new UniqueEntityID(raw.bestAnswerId) : null,
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
       },
       new UniqueEntityID(raw.id),
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
   }
 }
